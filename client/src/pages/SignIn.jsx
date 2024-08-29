862ca4ca2e41d84236c78d8589701624bc7127d9
Ham Petrosyan <hampetrosyan007@gmail.com>
2024-08-30 01:31:57 +0400

    complite sign in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,3 +1,92 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="flex flex-col justify-end min-h-[50vh] max-w-[400px] m-auto px-3">
+        <h1 className="text-3xl text-center font-semibold text-customDarkGreen my-7">
+          Sign In
+        </h1>
+        <form
+          autoComplete="off"
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 text-green-950"
+        >
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            onChange={handleChange}
+            className="border border-customDarkGreen focus:outline-customDarkGreen placeholder:text-customDarkGreen placeholder:opacity-40 p-3 rounded-full w-full"
+          />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            onChange={handleChange}
+            className="border border-customDarkGreen focus:outline-customDarkGreen placeholder:text-customDarkGreen placeholder:opacity-40 p-3 rounded-full w-full"
+          />
+          <button
+            disabled={loading}
+            className={`bg-green-900 text-white px-8 py-3 rounded-full transition-all duration-100 hover:opacity-90 disabled:opacity-80 uppercase ${
+              loading ? "cursor-not-allowed" : "cursor-pointer"
+            }`}
+          >
+            {loading ? "Loading..." : "Sign In"}
+          </button>
+        </form>
+        <div className="flex gap-2 pt-2">
+          <p className="text-green-950">Don&apos;t have an account?</p>
+          <Link to={"/sign-up"}>
+            <span className="text-customDarkGreen hover:underline hover:underline-offset-2">
+              Sign Up
+            </span>
+          </Link>
+        </div>
+      </div>
+      <div className="max-w-[400px] m-auto px-3">
+        {error && <p className="text-red-500 mt-3">{error}</p>}
+      </div>
+    </>
+  );
 }
