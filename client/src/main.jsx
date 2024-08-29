92c02cb0c77b0b4e2ceb3cc7ce1ab79471b5ac2f
Ham Petrosyan <hampetrosyan007@gmail.com>
2024-08-30 02:16:54 +0400

    edit main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 import { persistor, store } from "./redux/store.js";
 
 import App from "./App.jsx";
 import "./index.css";
-import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
